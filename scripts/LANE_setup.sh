1915ebff0a24858f8f1f418720fe1c7174427f9f
Kate Lane <30812314+kate-lane@users.noreply.github.com>
2021-12-04 20:56:01 -0500

    Update LANE_setup.sh

--- scripts/LANE_setup.sh
+++ scripts/LANE_setup.sh
@@ -1,3 +1,5 @@
 #Get started!
 #Clone this github repo to your HPC or local computing environment:
 git clone git@github.com:max-jahns/bioinformatics-final-2021.git
+
+#Note that these scripts are configured for SLURM cluster https://slurm.schedmd.com/, adjust as needed for your computational resources
