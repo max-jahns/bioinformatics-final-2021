55306cf6126dc98d1f502ea8f1ff453e33201772
kate-lane <katierose.lane@gmail.com>
2021-12-07 16:45:50 -0500

    update

--- scripts/LANE_setup.sh
+++ scripts/LANE_setup.sh
@@ -3,3 +3,5 @@
 git clone git@github.com:max-jahns/bioinformatics-final-2021.git
 
 #Note that these scripts are configured for SLURM cluster https://slurm.schedmd.com/, adjust as needed for your computational resources
+
+#These scripts will work best when run in sequence of numbers in names. They are subdivided into  scripts so that they may be run individually if needed
